83c6ae3712a46b16b1490d9b7a20a9ca5cb16468
Harry <harry.wheeleriv@live.com>
2015-10-12 21:07:12 -0700

    Minor update
    added some more syntax.

--- decreaseSearch.py
+++ decreaseSearch.py
@@ -1,2 +1,17 @@
 # this code will be used to search through the already filtered files
 # with identified magnetic field rotations.
+
+import os
+
+
+def Main():
+    datadirectory = 'D:\\Research\\scripts\\Results\\FullSet1\\'
+
+    
+if __name__ == '__main__':
+    #Main()
+
+    datadirectory = 'D:\\Research\\scripts\\Results\\FullSet1\\'
+    subdirectories = [row for row in os.listdir(datadirectory) if '$' not in row]
+    
+    
